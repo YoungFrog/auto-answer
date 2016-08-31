342170d1310fbf431c62550229bcfa31948f0a3c
Nicolas Richard <youngfrog@members.fsf.org>
2016-08-31 23:47:42 +0200

    Add version header to auto-answer.el

--- auto-answer.el
+++ auto-answer.el
@@ -4,6 +4,7 @@
 
 ;; Author: Nicolas Richard <youngfrog@members.fsf.org>
 ;; Keywords: convenience, elisp
+;; Version: 1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
